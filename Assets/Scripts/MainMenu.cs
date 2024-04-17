3baa8a55b02b4fe0a466fc72b2769e2b180b7000
vratislavino <vratislavino@gmail.com>
2024-04-17 13:06:05 +0200

    dú

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,17 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField]
+    private LevelButton levelButtonPrefab;
+
+    [SerializeField]
+    private Transform levelButtonsParent;
+
     // Start is called before the first frame update
     void Start()
     {
-        // vytvoøit list dostupných levelù
-        // pro každý level vytvoøit button
-        // na každém LevelButtonu zavolat SetData s názvem levelu
-        // Pøidat LevelButton jako prefab
-        // Pøidat všechny level buttony jako childy pro grid layout
+        var levels = GetAllLevels();
+        foreach(var level in levels)
+        {
+            var levelButton = Instantiate(levelButtonPrefab, levelButtonsParent);
+            levelButton.SetData(level);
+        }
+    }
+
+    private List<string> GetAllLevels()
+    {
+        List<string> levels = new List<string>();
+        for(int i = 0; i < SceneManager.sceneCountInBuildSettings; i++)
+        {
+
+            string path = SceneUtility.GetScenePathByBuildIndex(i);
+            string[] parts = path.Split('/');
+            string sceneName = parts[parts.Length - 1];
+            sceneName = sceneName.Replace(".unity", "");
+            if (sceneName.StartsWith("Level"))
+                levels.Add(sceneName);
+
+        }
+        return levels;
+
     }
 
     public void ExitGame()
